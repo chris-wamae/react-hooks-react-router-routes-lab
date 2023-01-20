a58f4e40b15ff2b3f92594fd2a7000b4b300ca8b
chris-wamae <chriswamae123@gmail.com>
2023-01-20 08:03:04 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,25 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
-
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar/>
+      <switch>
+        <Route path="/movies">
+          <Movies/>
+        </Route>
+        <Route path="/directors">
+          <Directors/>
+        </Route>
+        <Route path="/actors">
+          <Actors/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </switch>
+    </div>
+  );
 }
-
 export default App;
